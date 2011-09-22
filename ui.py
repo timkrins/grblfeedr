70c84036f8bfe75ef416fe384807866cd7fa932d
timkrins <timkrins@gmail.com>
2011-09-23 09:25:11 +1000

    Highlighting

--- ui.py
+++ ui.py
@@ -4,6 +4,39 @@ from PySide import QtCore, QtGui
 from outp import Ui_MainWindow
 from gcodeformat import GForm
 
+class MyHighlighter( QtGui.QSyntaxHighlighter ):
+    def __init__( self, parent ):
+      QtGui.QSyntaxHighlighter.__init__( self, parent )
+      self.parent = parent
+      self.highlightingRules = []
+
+      keyword = QtGui.QTextCharFormat()
+      keyword.setForeground( QtCore.Qt.darkBlue )
+      keyword.setFontWeight( QtGui.QFont.Bold )
+      keywords = [ "x", "X", "for", "if", "in",
+                                "next", "repeat", "return", "switch",
+                                "try", "while" ]
+      for word in keywords:
+        pattern = QtCore.QRegExp(word)
+        rule = HighlightingRule( pattern, keyword )
+        self.highlightingRules.append( rule )
+        
+    def highlightBlock( self, text ):
+      for rule in self.highlightingRules:
+        expression = QtCore.QRegExp( rule.pattern )
+        index = expression.indexIn( text )
+        while index >= 0:
+          length = expression.matchedLength()
+          self.setFormat( index, length, rule.format )
+          index = text.indexOf( expression, index + length )
+      self.setCurrentBlockState( 0 )
+
+        
+class HighlightingRule():
+  def __init__( self, pattern, format ):
+    self.pattern = pattern
+    self.format = format
+    
 class SetUp(Ui_MainWindow):
 
     def __init__(self, parent=None):
@@ -24,9 +57,10 @@ class SetUp(Ui_MainWindow):
         fLoad = open(fileName[0],'r')
         for line in fLoad:
             contents.append(line)
-            lined = Formatter.format_html(line)
+            lined = line+"<br>"
             formattedcontents.append(lined)
         fLoad.close()
+        highlighter = MyHighlighter( self.textEdit )
         self.textEdit.setHtml("".join(formattedcontents))
         
     def setupSlots(self, parent=None):
