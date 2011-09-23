1139a3c5baa0e5606cdc3526845f13bc6c80026f
timkrins <timkrins@gmail.com>
2011-09-23 17:29:50 +1000

    Works nicely! Ready to be tested on real CNC. Needs settings dialogue.

---
+++ Makefile
@@ -0,0 +1,2 @@
+outp.py : mainwindow.ui
+	python _pyuic.py mainwindow.ui -o outp.py
--- ui.py
+++ ui.py
@@ -1,91 +1,240 @@
-import sys
+import sys, time, serial
 from PySide import QtCore, QtGui
 
 from outp import Ui_MainWindow
 from gcodeformat import GForm
 
-class MyHighlighter( QtGui.QSyntaxHighlighter ):
-    def __init__( self, parent ):
-      QtGui.QSyntaxHighlighter.__init__( self, parent )
-      self.parent = parent
-      self.highlightingRules = []
-
-      keyword = QtGui.QTextCharFormat()
-      keyword.setForeground( QtCore.Qt.darkBlue )
-      keyword.setFontWeight( QtGui.QFont.Bold )
-      keywords = [ "x", "X", "for", "if", "in",
-                                "next", "repeat", "return", "switch",
-                                "try", "while" ]
-      for word in keywords:
-        pattern = QtCore.QRegExp(word)
-        rule = HighlightingRule( pattern, keyword )
-        self.highlightingRules.append( rule )
+class Worker(QtCore.QThread):
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+        self.exiting = False
+       
+    def run(self):
+        while not self.exiting:
+            time.sleep(0.01)
+            if(oksig[0] == True):
+                oksig[0] = False
+                self.emit(QtCore.SIGNAL("doney()"))
+                
+    def __del__(self, parent=None):
+        self.exiting = True
+        self.wait()
+            
+class SerialProcessor(QtCore.QThread):
+    def __init__(self, parent = None):
+        QtCore.QThread.__init__(self, parent)
+        self.exiting = False
         
-    def highlightBlock( self, text ):
-      for rule in self.highlightingRules:
-        expression = QtCore.QRegExp( rule.pattern )
-        index = expression.indexIn( text )
-        while index >= 0:
-          length = expression.matchedLength()
-          self.setFormat( index, length, rule.format )
-          index = text.indexOf( expression, index + length )
-      self.setCurrentBlockState( 0 )
-
+    def __del__(self, parent=None):
+        self.exiting = True
+        self.wait()
         
-class HighlightingRule():
-  def __init__( self, pattern, format ):
-    self.pattern = pattern
-    self.format = format
+    def processchunk(self, currentline):
+        popped = []
+        while ("\n" in currentline) or ("\r" in currentline):
+            currentpop = currentline.pop(0)
+            popped.extend(currentpop)
+        else:
+            if(popped):
+                popped.pop()
+                if(len(popped) > 1):
+                    if("".join(popped) == "ok"):
+                        oksig[0] = True
+                        #print("got okay!")
+                        termWindowList.append("<font color=lime><i>["+("".join(popped))+"]</i></font>")
+                    else:
+                        termWindowList.append("<font color=yellow>["+("".join(popped))+"]</font>")
+                    self.emit(QtCore.SIGNAL("regen()"))    
+                popped = []
+        return ""
     
+    def run(self):
+        a = 0
+        parent.ser.flushInput()
+        currentline = []
+        while not self.exiting:
+            time.sleep(0.01)
+            a += 1
+            if(True):
+                #print("processing chunk serial "+repr(a))
+                self.processchunk(currentline)
+                chunked = ser.readline()
+                if(chunked):
+                    currentline.extend(list(chunked.decode('utf-8')))
+            
 class SetUp(Ui_MainWindow):
 
     def __init__(self, parent=None):
         Ui_MainWindow.__init__(self)
         
+    def setupDefaults(self, parent=None):
+        self.termLine.setCursorPosition(2)
+        self.currentProgress.setValue(0)
+        self.senderThread = Worker()
+        self.sendStop.setDisabled(True)
+        self.clearButton.setDisabled(True)
+        self.sendOne.setDisabled(True)
+        self.sendCont.setDisabled(True)
+        parent.resize(500, 305)
+        self.termWindow.setHtml("")
+        self.serialThread = SerialProcessor()
+        self.serialThread.start()
         
-    def clearInput(self):
+    def clearInput(self, parent=None):
         if(self.termLine.text() == "Enter Command"):
             self.termLine.clear()
             
-    def hitEnter(self):
-        print(self.termLine.text())
+    def hitEnter(self, parent=None):
+        termWindowList.append("<font color=lime>"+self.termLine.text()+"</font>")
+        ser.write((self.termLine.text()+"\n").encode('utf-8'))
+        self.regenTerminalWindow()
         self.termLine.clear()
 
-    def openFile(self):
+    def openFile(self, parent=None):
         fileName = QtGui.QFileDialog.getOpenFileName(None, u"Select G-Code to send to CNC",u"",u"GCode Files (*.nc *.gc);;All Files (*.*)")
-        #print(fileName[0])
         fLoad = open(fileName[0],'r')
+        del(contents[:])
+        del(formattedcontents[:])
+        self.clearButton.setDisabled(False)
+        self.sendOne.setDisabled(False)
+        self.sendCont.setDisabled(False)
+        outerdisplay.resize(760, 305)
         for line in fLoad:
             contents.append(line)
             lined = line+"<br>"
             formattedcontents.append(lined)
         fLoad.close()
-        highlighter = MyHighlighter( self.textEdit )
-        self.textEdit.setHtml("".join(formattedcontents))
+        self.origlength = len(contents)
+        self.regenContentWindow()
         
+    def click_sendOne(self, parent=None):
+        currentliner = contents.pop(0)
+        del(formattedcontents[0:3])
+        currentline[0] += 1.0
+        self.regenContentWindow()
+        termWindowList.append("<font color=red>"+currentliner+"</font>")
+        ser.write((currentliner).encode('utf-8'))
+        self.regenTerminalWindow()
+        
+    def click_sendCont(self, parent=None):
+        self.senderThread.start()
+        self.sendStop.setDisabled(False)
+        self.sendCont.setDisabled(True)
+        self.sendOne.setDisabled(False)
+        self.loadButton.setDisabled(True)
+        
+    def com_threadSender(self, parent=None):
+        try:
+            currentliner = contents.pop(0)
+            del(formattedcontents[0:3])
+            currentline[0] += 1.0
+            self.regenContentWindow()
+            termWindowList.append("<font color=blue>"+currentliner+"</font>")
+            ser.write(currentliner.encode('utf-8'))
+            self.regenTerminalWindow()
+        except:
+            self.senderThread.terminate()
+          
+    def click_clearQueue(self, parent=None):
+        del(contents[:])
+        del(formattedcontents[:])
+        self.sendCont.setDisabled(True)
+        self.sendOne.setDisabled(True)
+        self.clearButton.setDisabled(True)
+        self.origlength = 1
+        self.regenContentWindow()
+        termWindowList.append("<font color=yellow>"+"-> queue cleared..."+"</font>")
+        self.regenTerminalWindow()
+        self.currentProgress.setValue((0))
+
+    def click_clearTerm(self, parent=None):
+        del(termWindowList[:])
+        self.regenTerminalWindow()
+
+    def com_stopCont(self, parent=None):
+        termWindowList.append("<font color=yellow>"+"-> stopping..."+"</font>")
+        self.regenTerminalWindow()
+        self.sendStop.setDisabled(True)
+        self.sendCont.setDisabled(False)
+        self.sendOne.setDisabled(False)
+        self.loadButton.setDisabled(False)
+        
+    def click_sendStop(self, parent=None):
+        self.senderThread.terminate()
+        
+    def exitAll(self, parent=None):
+            if(self.senderThread.exiting == False):
+                self.senderThread.terminate()
+            if(self.serialThread.exiting == False):
+                self.serialThread.terminate()
+            sys.exit()
+
+    def click_actionConnect(self, parent=None):
+        pass
+
+    def term_serialThread(self, parent=None):
+        pass
+
+    def regenContentWindow(self, parent=None): 
+        try:
+            if(formattedcontents[1]):
+                formattedcontents.insert(1,"</font><font color=grey>")
+                formattedcontents.insert(0,"<font color=red size=4><b>>> </font><font color=blue size=4>")
+                formattedcontents.append("</font>")
+        except:
+            pass
+            #formattedcontents.append("Queue Empty")
+        self.textEdit.setHtml("".join(formattedcontents[0:30]))
+        self.currentProgress.setValue((currentline[0]/self.origlength)*100)
+
+    def regenTerminalWindow(self, parent=None):   
+        self.termWindow.setHtml("<br>".join(termWindowList[-13:]))
+
     def setupSlots(self, parent=None):
-        QtCore.QObject.connect(self.exitButton, QtCore.SIGNAL("clicked()"), sys.exit)
+        QtCore.QObject.connect(self.exitButton, QtCore.SIGNAL("clicked()"), self.exitAll)
         QtCore.QObject.connect(self.loadButton, QtCore.SIGNAL("clicked()"), self.openFile)
-        self.termLine.setCursorPosition(2)
+        QtCore.QObject.connect(self.actionLoad_GCode, QtCore.SIGNAL("activated()"), self.openFile)
+        QtCore.QObject.connect(self.sendOne, QtCore.SIGNAL("clicked()"), self.click_sendOne)
+        QtCore.QObject.connect(self.sendCont, QtCore.SIGNAL("clicked()"), self.click_sendCont)
+        QtCore.QObject.connect(self.sendStop, QtCore.SIGNAL("clicked()"), self.click_sendStop)
         QtCore.QObject.connect(self.termLine, QtCore.SIGNAL("cursorPositionChanged(int,int)"), self.clearInput)
         QtCore.QObject.connect(self.termLine, QtCore.SIGNAL("returnPressed()"), self.hitEnter)
+        QtCore.QObject.connect(self.senderThread, QtCore.SIGNAL("terminated()"), self.com_stopCont)
+        QtCore.QObject.connect(self.senderThread, QtCore.SIGNAL("doney()"), self.com_threadSender)
+        QtCore.QObject.connect(self.clearButton, QtCore.SIGNAL("clicked()"), self.click_clearQueue)
+        QtCore.QObject.connect(self.clearTerm, QtCore.SIGNAL("clicked()"), self.click_clearTerm)
+        QtCore.QObject.connect(self.actionConnect, QtCore.SIGNAL("activated()"), self.click_actionConnect)
+        QtCore.QObject.connect(self.serialThread, QtCore.SIGNAL("terminated()"), self.term_serialThread)
+        QtCore.QObject.connect(self.serialThread, QtCore.SIGNAL("regen()"), self.regenTerminalWindow)
 
 
+#actionLoad_GCode
 #QLineEdit.focusInEvent (self, QFocusEvent)
 
-def main():
+#def main():
 
-    app = QtGui.QApplication(sys.argv)
-    outerdisplay = QtGui.QMainWindow()
-    window = SetUp(app)
-    window.setupUi(outerdisplay)
-    window.setupSlots(outerdisplay)
-    outerdisplay.show()
-    sys.exit(app.exec_())
+
+
 
 contents = []
+termWindowList =  []
 formattedcontents = []
+continueRunning = []
+oksig = [1]
+origlength = 0
+currentline = [0]
 Formatter = GForm()
 
-if __name__ == '__main__':
-    main()
+ser = serial.Serial('COM10',9600,8,'N',1,0.01)
+
+app = QtGui.QApplication(sys.argv)
+outerdisplay = QtGui.QMainWindow()
+window = SetUp(app)
+window.setupUi(outerdisplay)
+window.setupDefaults(outerdisplay)
+window.setupSlots(outerdisplay)
+outerdisplay.show()
+sys.exit(app.exec_())
+    
+#if __name__ == '__main__':
+#    main()
